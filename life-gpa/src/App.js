99d5a5a7b8c10764b8ad2b1ffc2631293d62b1d9
Megan Jones <38929577+meginks@users.noreply.github.com>
2019-03-18 15:56:08 -0500

    set up redux files

--- life-gpa/src/App.js
+++ life-gpa/src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import Dashboard from './components/Dashboard';
+import {BrowserRouter as Router} from 'react-router-dom';
+
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="App">
        <h1>Life GPA</h1>
        <Dashboard /> 
       </div>
+      </Router>
     );
   }
 }
--- life-gpa/src/index.js
+++ life-gpa/src/index.js
@@ -2,10 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {BrowserRouter as Router} from 'react-router-dom';
+import rootReducer from './reducers';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+
 
 ReactDOM.render(
-<Router>
+<Provider store={store}>
 <App />
-</Router>, document.getElementById('root'));
+</Provider>
+, document.getElementById('root'));
 
